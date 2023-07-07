b400b25c6cf79ec6f701a441b29df7e8f73be0a3
Rupa Dey <68509719+Rupa-Dey@users.noreply.github.com>
2023-07-07 23:09:37 +0600

    Update first_fit.cpp

--- OSlab/lab/first_fit.cpp
+++ OSlab/lab/first_fit.cpp
@@ -2,28 +2,45 @@
 using namespace std;
  int main()
  {
-     freopen("input.txt","r",stdin);
+    // freopen("input.txt","r",stdin);
      int n,i,j=1,k=0,q;
      cin>>n;
      int p = n;
-     vector<int>v(n),ans(n);
+     vector<int>v(n),ans;
      for(i=0;i<n;i++){
         cin>>v[i];
      }
+    vector<pair<int,int>>vi,x;
+    
 
+    for(i=0;i<n;i++){
+        vi.push_back(make_pair(v[i],i+1));
+    }
+
+    sort(vi.begin(),vi.end());
+   
+    for(auto it:vi) cout<<it.first<<":"<<it.second<<'\n';
+    
+   int t=0;
      while(p--) {
             cin>>q;
             bool chk = false;
-        for(i=0;i<n;i++){
-        if(v[i]>=q){
-                cout<<"allocated into "<<v[i]<<"\n";
+        for(i=0;i<vi.size();i++){
+        if(vi[i].first>=q){
+
+                cout<<"allocated into "<<vi[i].first<<"\n";
             chk  = true;
-            ans[i] = k++;
-            v[i] = -1;
+            ans.push_back(vi[i].second);
+            
+            vi.erase(vi.begin()+i);
+            //cout<<t<<endl;
+       
             break;
-        } }
+        }
+        else continue; }
         if(!chk) cout<<"no allocation"<<endl;
-     }
-     for(auto it:ans) cout<<it+1<<" ";
+      }
+     for(auto it:ans) cout<<it<<" ";
      cout<<endl;
  }
+
