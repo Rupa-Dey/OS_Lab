1506275b5e4b36ea372727eb4921f082a8ffeeb7
Rupa Dey <68509719+Rupa-Dey@users.noreply.github.com>
2023-07-07 23:06:49 +0600

    Updated the code
    actually here is occurred the Gantt chart problem

--- OSlab/lab/best_fit.cpp
+++ OSlab/lab/best_fit.cpp
@@ -2,7 +2,7 @@
 using namespace std;
  int main()
  {
-     freopen("input.txt","r",stdin);
+    // freopen("input.txt","r",stdin);
      int n,i,j=1,k=0,q;
      cin>>n;
      int p = n;
@@ -10,26 +10,31 @@ using namespace std;
      for(i=0;i<n;i++){
         cin>>v[i];
      }
-    vector<pair<int,int>>vi;
+    vector<pair<int,int>>vi,x;
+    
 
     for(i=0;i<n;i++){
         vi.push_back(make_pair(v[i],i+1));
     }
+
     sort(vi.begin(),vi.end());
+   
     for(auto it:vi) cout<<it.first<<":"<<it.second<<'\n';
+    
    int t=0;
      while(p--) {
             cin>>q;
             bool chk = false;
-        for(auto it:vi){
-        if(it.first>=q){
+        for(i=0;i<vi.size();i++){
+        if(vi[i].first>=q){
 
-                cout<<"allocated into "<<it.first<<"\n";
+                cout<<"allocated into "<<vi[i].first<<"\n";
             chk  = true;
-            ans.push_back(it.second);
-            vi.erase(vi.begin());
+            ans.push_back(vi[i].second);
+            
+            vi.erase(vi.begin()+i);
             //cout<<t<<endl;
-       // t++;
+       
             break;
         }
         else continue; }
